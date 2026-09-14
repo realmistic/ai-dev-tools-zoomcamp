aac019a9f65025a6b1076739672a71fabb3aa302
Ivan Brigida <realmistic@gmail.com>
2026-09-14 22:32:34 +0100

    Add 10s timeout to API client to prevent hanging

--- fairshare_app/api_client.py
+++ fairshare_app/api_client.py
@@ -11,7 +11,7 @@ class APIClient:
 
     def __init__(self, base_url: str = API_BASE_URL):
         self.base_url = base_url
-        self.client = httpx.Client(base_url=base_url)
+        self.client = httpx.Client(base_url=base_url, timeout=10.0)
 
     def create_group(self, trip_name: str) -> Dict:
         """Create a new group."""
